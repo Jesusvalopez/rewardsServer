799a98104c490e6e1cf1f514e9ea1b30a985ffb9
jesus <jlv@inpact.net>
2021-03-12 01:16:54 -0300

    avance

--- models/coupon.js
+++ models/coupon.js
@@ -1,21 +1,20 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 const couponSchema = mongoose.Schema({
-
-    value: Number,
-    creator: String,
-    user: String,
-    status: String,
-    createdAt:{
-        type: Date,
-        default: new Date()
-    },
-    expireDate:{
-        type: Date
-    }
-
+  value: Number,
+  creator: String,
+  user: String,
+  status: String,
+  type: String,
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+  expireDate: {
+    type: Date,
+  },
 });
 
-const Coupon = mongoose.model('Coupon', couponSchema);
+const Coupon = mongoose.model("Coupon", couponSchema);
 
 export default Coupon;
