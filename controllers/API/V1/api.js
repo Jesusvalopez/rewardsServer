a15983d250dae20b8b1ad265fd1bfbd97c0c59ce
jesus <jlv@inpact.net>
2021-03-19 00:28:15 -0300

    rutas de api

--- controllers/API/V1/api.js
+++ controllers/API/V1/api.js
@@ -1,5 +1,25 @@
 import User from "../../../models/user.js";
+import Coupon from "../../../models/coupon.js";
 
+export const getUsersCoupons = async (req, res) => {
+  try {
+    console.log(req.params.id);
+    const user = await User.findOne({
+      _id: req.params.id,
+    });
+
+    const coupons = await Coupon.find({
+      user: user.email,
+      deletedAt: { $eq: null },
+      isExpired: { $eq: false },
+      isUsed: { $eq: false },
+    });
+
+    return res.status(200).json({ coupons });
+  } catch (error) {
+    return res.status(404).json({ message: error.message });
+  }
+};
 export const getUsersByEmail = async (req, res) => {
   try {
     const users = await User.find({
--- routes/API/V1/api.js
+++ routes/API/V1/api.js
@@ -1,9 +1,13 @@
 import express from "express";
 
-import { getUsersByEmail } from "../../../controllers/API/V1/api.js";
+import {
+  getUsersByEmail,
+  getUsersCoupons,
+} from "../../../controllers/API/V1/api.js";
 import apiKeyAuth from "../../../middleware/apiKeyAuth.js";
 const router = express.Router();
 
 router.get("/get-users/:email", apiKeyAuth, getUsersByEmail);
+router.get("/get-users-coupons/:id", apiKeyAuth, getUsersCoupons);
 
 export default router;
